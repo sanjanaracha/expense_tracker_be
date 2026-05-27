993c1837cefad9b9748e0e3e8cb99f2a7d634107
sanjanaracha <sanjanaracha16@gmail.com>
2026-05-27 23:55:42 +0530

    cors added

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ conn = mysql.connector.connect(
     host=os.getenv("db_host"),
     user=os.getenv("db_user"),
     password=os.getenv("db_password"),
-    database=os.getenv("db_name"),
+    database=os.getenv("db_database"),
     port=int(os.getenv("db_port"))
 )
 
